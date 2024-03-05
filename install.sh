a61341654488b23a9958a12584e41f3ca820020e
Johan Rendón Coronado <66027065+Johan-Rendon@users.noreply.github.com>
2024-03-05 15:02:11 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #notes
 curl -Ls https://raw.githubusercontent.com/pimterry/notes/latest-release/install.sh | sudo bash
 
-sudo pacman -S neovim alacritty fish starship tmux
+sudo pacman -S neovim alacritty fish starship tmux stow
 
 # Stow
 stow alacritty/
