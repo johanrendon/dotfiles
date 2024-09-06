ca34e3622493ff26b25d382e35fc756a8e52af4b
Johan Rendon <johanrendo1@gmail.com>
2024-09-06 13:51:13 -0500

    feat(nvim)!:change all the config

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "catppuccin",
+					globalstatus = true,
+					component_separators = { left = "", right = "" },
+					section_separators = { left = "█", right = "█" },
+				},
+				sections = {
+					lualine_b = {
+						"diff",
+						"diagnostics",
+					},
+					lualine_c = {
+						{ "filename", path = 1 },
+					},
+					lualine_x = {
+						"filetype",
+					},
+				},
+			})
+		end,
+	},
+}
