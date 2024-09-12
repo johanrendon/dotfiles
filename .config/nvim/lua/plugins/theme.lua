bae229818536cf28ae8f2595342ad7522e66350a
Johan Rendon <johanrendo1@gmail.com>
2024-09-11 19:13:42 -0500

    feat(nvim): Change wilder plugin for noise

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,8 +1,6 @@
 return {
 	-- add dracula
-	{ "Mofiqul/dracula.nvim", opts = {
-		transparent_bg = true,
-	} },
+	{ "Mofiqul/dracula.nvim", opts = {} },
 	"folke/tokyonight.nvim",
 	"ellisonleao/gruvbox.nvim",
 	"navarasu/onedark.nvim",
