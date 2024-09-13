0c839249e5a8e255d17aff31ffc8da30f97f1094
Johan Stiven Rendón Coronado <johanrendo1@gmail.com>
2024-09-13 01:36:23 -0500

    refactor(nvim): Change the keys for the debug plugin

--- .config/nvim/lua/plugins/debug.lua
+++ .config/nvim/lua/plugins/debug.lua
@@ -28,9 +28,9 @@ return {
 			{ "<F1>", dap.step_into, desc = "Debug: Step Into" },
 			{ "<F2>", dap.step_over, desc = "Debug: Step Over" },
 			{ "<F3>", dap.step_out, desc = "Debug: Step Out" },
-			{ "<C-b>", dap.toggle_breakpoint, desc = "Debug: Toggle Breakpoint" },
+			{ "<F4>", dap.toggle_breakpoint, desc = "Debug: Toggle Breakpoint" },
 			{
-				"<C-B>",
+				"<F6>",
 				function()
 					dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
 				end,
