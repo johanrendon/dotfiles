5453c231c218207554c12fa81d3b0cde2b7f0e3c
Johan Stiven Rendón Coronado <johanrendo1@gmail.com>
2024-09-17 17:07:58 -0500

    feat(scripts): Script for download kaggle competitions

---
+++ scripts/utils/datascience/download_competition.py
@@ -0,0 +1,47 @@
+import os
+import subprocess
+import zipfile
+from pathlib import Path
+from typing import Annotated, List
+
+import typer
+from loguru import logger
+from tqdm import tqdm
+
+app = typer.Typer()
+
+
+@app.command()
+def main(
+    data_dir: Annotated[Path, typer.Argument(help="Data directory")],
+    competition: Annotated[str, typer.Argument(help="Competition to download")],
+):
+    kaggle_command: List[str | Path] = [
+        "kaggle",
+        "competitions",
+        "download",
+        "-c",
+        competition,
+        "-p",
+        data_dir,
+    ]
+
+    subprocess.run(kaggle_command)
+
+    data_path: Path = data_dir / f"{ competition }.zip"
+    logger.info("Download kaggle zip")
+
+    with zipfile.ZipFile(data_path, "r") as zip_data:
+        zip_data.extractall(path=data_dir)
+
+    if data_path.exists():
+        os.remove(data_path)
+
+    # .csv
+    for file in os.listdir(data_dir):
+        if file.strip(".")[-1] != "csv":
+            os.remove(data_dir / file)
+
+
+if __name__ == "__main__":
+    app()
