57f56cf77403435e6f60e20bf52b242934a357d0
Johan Stiven Rendón Coronado <johanrendo1@gmail.com>
2024-10-03 10:24:09 -0500

    chroe: add zig format

--- .config/nvim/lua/plugins/comform.lua
+++ .config/nvim/lua/plugins/comform.lua
@@ -33,8 +33,8 @@ return { -- Autoformat
 		formatters_by_ft = {
 			lua = { "stylua" },
 			-- Conform can also run multiple formatters sequentially
-
 			python = { "ruff_organize_imports", "ruff_format" },
+			zig = { "zig fmt" },
 			--
 			-- You can use 'stop_after_first' to run the first available formatter from the list
 		},
